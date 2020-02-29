6e7df00024d7966e01f63cf33a52e02238fa80f6
Sayandh <sayandh@acube.co>
2020-02-29 22:07:02 +0530

    final

---
+++ SharedPreference.kt
@@ -0,0 +1,80 @@
+package com.kotlincodes.carewash.utils
+
+import android.content.Context
+import android.content.SharedPreferences
+import com.securepreferences.SecurePreferences
+
+/**
+ * Created by Sayandh-Kolincodes on 10/05/2018.
+ */
+
+class SharedPreference(val context: Context) {
+    private val PREFS_NAME = "your_app_preference_name"
+
+//    private val sharedPref: SecurePreferences = SecurePreferences(context)
+    private val sharedPref: SharedPreferences =
+        context.getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE)
+
+    fun save(KEY_NAME: String, text: String) {
+
+        val editor: SharedPreferences.Editor = sharedPref.edit()
+
+        editor.putString(KEY_NAME, text)
+
+        editor.commit()
+    }
+
+    fun save(KEY_NAME: String, value: Int) {
+        val editor: SharedPreferences.Editor = sharedPref.edit()
+
+        editor.putInt(KEY_NAME, value)
+
+        editor.commit()
+    }
+
+    fun save(KEY_NAME: String, status: Boolean) {
+
+        val editor: SharedPreferences.Editor = sharedPref.edit()
+
+        editor.putBoolean(KEY_NAME, status)
+
+        editor.commit()
+    }
+
+    fun getValueString(KEY_NAME: String): String? {
+
+        return sharedPref.getString(KEY_NAME, null)
+
+
+    }
+
+    fun getValueInt(KEY_NAME: String): Int {
+
+        return sharedPref.getInt(KEY_NAME, 0)
+    }
+
+    fun getValueBoolien(KEY_NAME: String, defaultValue: Boolean): Boolean {
+
+        return sharedPref.getBoolean(KEY_NAME, defaultValue)
+
+    }
+
+    fun clearSharedPreference() {
+
+        val editor: SharedPreferences.Editor = sharedPref.edit()
+
+        //sharedPref = PreferenceManager.getDefaultSharedPreferences(context);
+
+        editor.clear()
+        editor.commit()
+    }
+
+    fun removeValue(KEY_NAME: String) {
+
+        val editor: SharedPreferences.Editor = sharedPref.edit()
+
+        editor.remove(KEY_NAME)
+        editor.commit()
+    }
+
+}
